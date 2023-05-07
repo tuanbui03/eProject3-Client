d5f27e2213459c1ed815032810610efa03b2e1f5
tuanbui03 <tuanbui.10103@gmail.com>
2023-05-07 13:41:13 +0700

    first commit

---
+++ ABCD_Client/ABCD_Client/Controllers/CinemaController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ABCD_Client.Controllers
+{
+    public class CinemaController : Controller
+    {
+        // GET: Cinema
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Bùi Anh Tuấn test layout book vé
+        public ActionResult BookingTickets()
+        {
+            return View();
+        }
+
+        // GET: Cinema/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Cinema/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cinema/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cinema/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Cinema/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Cinema/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Cinema/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
