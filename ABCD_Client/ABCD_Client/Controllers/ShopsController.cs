88bba5b04bb52b31ec0a7edc042124d2bbebb4c7
tuanbui03 <tuanbui.10103@gmail.com>
2023-05-09 14:06:17 +0700

    lỗi ở view/home/shop.cshtml

---
+++ ABCD_Client/ABCD_Client/Controllers/ShopsController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ABCD_Client.Models;
+
+namespace ABCD_Client.Controllers
+{
+    public class ShopsController : Controller
+    {
+        private Entities db = new Entities();
+
+        // GET: Shops
+        public ActionResult Index()
+        {
+            return View(db.Shops.ToList());
+        }
+
+        // GET: Shops/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shops shops = db.Shops.Find(id);
+            if (shops == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shops);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
